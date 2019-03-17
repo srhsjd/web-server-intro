6b68b27bad675a1b69564bf0cd02914a62765577
Guy John <guy@rumblesan.com>
2019-03-17 19:04:02 +0000

    Adding code from class

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var port = 3000
 
 var app = express()
 
+var state = {
+  name: "guy",
+  job: "programmer"
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'))
@@ -12,9 +17,24 @@ app.get('/', function (req, res) {
   res.sendFile('public/index.html');
 });
 
+app.get('/careers', function (req, res) {
+  res.sendFile(__dirname + '/public/careers.html');
+});
+
+app.get('/data', function (req, res) {
+  res.json(state);
+});
+
+app.post('/add', function (req, res) {
+  state.name = req.body.name;
+  state.job = req.body.job;
+  console.log(req.body);
+  res.send('OK');
+});
+
 app.get('/query', function (req, res) {
   console.log(req.query);
-  res.send('Another One');
+  res.json();
 });
 
 app.post('/somedata', function (req, res) {
